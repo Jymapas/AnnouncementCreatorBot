7cc56e16e2fa2f75c3f1e72910c7e3ccacad7b87
Jymapas <Jymapas@yandex.ru>
2023-06-28 17:03:00 +0300

    Попытка создания класса анонсов

---
+++ Announcement.cs
@@ -0,0 +1,17 @@
+﻿namespace AnnouncementCreatorBot
+{
+    public class Announcement
+    {
+        public Announcement() { }
+        public string tournamentName { get; init; }
+        public List<string> editors {get; init; }
+        public string eventDate { get; init; }
+        public string regTime {get; init; }
+        public string startTime { get; init; }
+        public string narrator { get; init; }
+        public string narratorsSex { get; init; }
+        public int payment { get; init; }
+        public int toursCount { get; init; }
+        public int questionsInTour { get; init; }
+    }
+}
